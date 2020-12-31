b37b6af91f3f21479b4b818d53208ef73066b998
jfernandezirujo <jfernandezirujo@gmail.com>
2020-12-31 12:02:49 -0300

    Empty project

---
+++ Trivia/Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Josefina Perez on 31/12/2020.
+//  Copyright © 2020 Julieta Fernández Irujo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
